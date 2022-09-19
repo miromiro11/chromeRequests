6ce993b9ddd1ec3c18dea20df1401f6bc3f461b2
Miro <64688403+miromiro11@users.noreply.github.com>
2022-09-18 17:27:18 -0700

    Update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chromeRequests',
-    version='0.1.7',
+    version='0.1.8',
     author="Miro",
     license='MIT',
     packages=find_packages(),
